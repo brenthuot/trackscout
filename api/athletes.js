11eda1debb7efd9a1204bf9d818411ef076c5395
brenthuot <brenthuot@gmail.com>
2026-03-01 01:32:27 -0500

    Update athletes.js

--- api/athletes.js
+++ api/athletes.js
@@ -29,6 +29,7 @@ export default async function handler(req, res) {
         )
       `)
       .eq("source", "tfrrs")
+      .neq("field_only", true)
       .range(offset, offset + limit - 1)
       .order("id");
 
